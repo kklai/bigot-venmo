c83e5396e51c588bab42110e82b3354225fb8c7a
kklai <kkrebeccalai@gmail.com>
2015-05-16 15:31:10 -0400

    test testtt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var request = require('request')
 
+var access_token = require('./public/token.js')
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-
-  var id, access_token, first_name;
+  var id, first_name;
 
   signIn();
 
